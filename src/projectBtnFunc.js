5b791e698a19ec6db5d0c4953f52d646f3fe88b7
JurreWindows <jurreveijk13@gmail.com>
2024-02-13 12:55:25 +0100

    added modal func

--- src/projectBtnFunc.js
+++ src/projectBtnFunc.js
@@ -1,15 +1,37 @@
 export function addProjectBtnFunc() {
     let projectbuttons = document.getElementsByClassName("projectbuttons");
-    
+    //Helper function to open the modal
+    function openModal() {
+        let showprojectmodal = document.getElementById("visibleprojectmodal");
+        if (showprojectmodal) {
+            showprojectmodal.style.display = "block";
+        }
+    }
+    //helperfunction to close the modal
+    function closeModal() {
+        let showprojectmodal = document.getElementById("visibleprojectmodal");
+        showprojectmodal.style.display = "none"
+    }
+    //func that loops through the created buttons, adds a single eventlistener,
+    //that when clicked will open the modal and when the red btn is clicked closes.
     for (let i = 0; i < projectbuttons.length; i++) {
-        projectbuttons[i].addEventListener('click', function() {
-            console.log("krijg kanker");
-            let showprojectdiv = document.createElement("div")
-            showprojectdiv.setAttribute("id", "visibleprojectdiv") 
-            let maincontent = document.getElementById("maincontent")
-            maincontent.append(showprojectdiv)
-
+        if (!projectbuttons[i].hasAttribute("data-clicked")) {
+            projectbuttons[i].setAttribute("data-clicked", "true");
             
-        });
+            projectbuttons[i].addEventListener('click', function() {
+                let showprojectmodal = document.getElementById("visibleprojectmodal");
+                if (!showprojectmodal) {
+                    showprojectmodal = document.createElement("div");
+                    showprojectmodal.setAttribute("id", "visibleprojectmodal");
+                   let projectmodalclosebtn = document.createElement("button")
+                    projectmodalclosebtn.classList.add("projectmodalclosebtn")
+                    projectmodalclosebtn.addEventListener("click", closeModal)
+                    showprojectmodal.append(projectmodalclosebtn)
+                    let maincontent = document.getElementById("todos");
+                    maincontent.appendChild(showprojectmodal);
+                }
+                openModal();
+            });
+        }
     }
 }
