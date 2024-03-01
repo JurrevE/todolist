667beee0abd813947a85b13e02597d720bf2fdac
Jurre van Eijk <jurreveijk13@gmail.com>
2024-03-01 10:32:38 +0100

    createse new id's

--- src/createProject.js
+++ src/createProject.js
@@ -1,8 +1,11 @@
 export function createProject() {
+    let projectcounter = 0
     class project {
         constructor (title) {
             this.title = title
             this.todos = []
+            this.id = "proj" + projectcounter
+            projectcounter++
         }
         addTodo(todo) {
             this.todos.push(todo);
