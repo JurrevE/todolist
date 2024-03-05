99343cb4428972394d1a6b652d93400b9b7be525
Jurre van Eijk <jurreveijk13@gmail.com>
2024-03-05 10:45:03 +0100

    now able to delete todos

--- src/deleteTodoButtonFunc.js
+++ src/deleteTodoButtonFunc.js
@@ -1,11 +1,20 @@
-
+import { displaytodos } from "./displaytodos";
+import differentprojects from ".";
 export function deleteTodoButtonFunc(){
     let deleteTodoButtons = document.getElementsByClassName("delete-todo-btn")
+    let projectName = document.getElementById("projectmodalname").innerText;
+    let project = differentprojects[projectName];
+    let todos = project.todos
 
+    //welke value zit vast aan welke knop
     for (let i = 0; i < deleteTodoButtons.length; i++) {
         deleteTodoButtons[i].addEventListener("click", function(event){
             let buttondata = event.target.getAttribute("data-delete-button")
             console.log(buttondata)
+            buttondata = todos[0]
+            todos.splice(buttondata, 1)
+            console.log(todos)
+            displaytodos()
         })
     }
     
